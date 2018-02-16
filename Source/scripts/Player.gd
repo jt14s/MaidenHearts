1a846f2fc85274c45555153d4a4f44d1b74d9234
jt14s <jt14s@my.fsu.edu>
2018-02-16 01:27:05 -0500

    Added 4 Way Sprite Update

--- Source/scripts/Player.gd
+++ Source/scripts/Player.gd
@@ -5,25 +5,40 @@ var MAX_SPEED = 600
 var velocity = Vector2()
 
 onready var sprite = get_node("Sprite")
+onready var spriteUp = preload("res://assets/tiles/standinup.png")
+onready var spriteDownAndRight = preload("res://assets/tiles/standin.png")
+onready var spriteLeft = preload("res://assets/tiles/standinleft.png")
 
 func _ready():
 	set_physics_process(true)
 	
 func _physics_process(delta):
+	### direction calculation ###
+	#
 	var direction = Vector2()
 	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	
+	### sprite update ###
+	#
+	### up and down
+	if direction.y < 0:
+		sprite.texture = spriteUp
+	elif direction.y > 0:
+		sprite.texture = spriteDownAndRight
+	### left and right
+	if direction.x < 0:
+		sprite.texture = spriteLeft
+	elif direction.x > 0:
+		sprite.texture = spriteDownAndRight
+	
+	### movement ###
+	#
 	if direction:
 		speed = MAX_SPEED
 	else:
 		speed = 0
-	
-	if direction.x < 0:
-		sprite.set_flip_h(true)
-	elif direction.x > 0:
-		sprite.set_flip_h(false)
-	
+		
 	velocity = speed * direction.normalized() * delta
 	move_and_collide(velocity)
 	
